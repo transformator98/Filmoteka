229036d25263b3eced8d589521ce0d025b700212
transformator98 <vanushka16@gmail.com>
2020-10-27 01:00:53 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+import './sass/base.scss';
+import './sass/main.scss';
+import './sass/normalize.scss';
+import './sass/reset.scss';
+
+import './sass/components/detailsPage.scss';
+import './sass/components/footer.scss';
+import './sass/components/header.scss';
+import './sass/components/homePage.scss';
+import './sass/components/main.scss';
+import './sass/components/myFilmLibraryPage.scss';
