16098681edc49b4ad8cae5f88cbd83cff05187b9
transformator98 <vanushka16@gmail.com>
2020-10-28 00:51:22 +0200

    adedd templates

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,33 @@ const browserSync = require('browser-sync').create();
 const tasks = requireDir('./gulp/tasks', { recurse: true });
 const paths = require('./gulp/paths');
 
+/***
+ * hendlebars
+ *
+ */
+var gulp = require('gulp');
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
+
+gulp.task('templates', function () {
+  gulp
+    .src('src/templates/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(
+      declare({
+        namespace: 'MyApp.templates',
+        noRedeclare: true, // Avoid duplicate declarations
+      }),
+    )
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('build/js/'));
+});
+
+//*** */
+
 const serve = () => {
   return browserSync.init({
     server: 'build',
