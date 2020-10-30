d64291e8d53007626f34c53d6de369e1e108ae9c
anastasiia-kolieva <nastya1110@gmail.com>
2020-10-30 17:32:06 +0200

    made function searchFilms

--- src/js/2searchAndPlaginationHomePage.js
+++ src/js/2searchAndPlaginationHomePage.js
@@ -10,27 +10,35 @@ const searchForm = document.querySelector('form.search-form');
 // ссылка на инпут
 const $input = document.querySelector('.search-form__input');
 
-
 // На форму поставила слушатель событий
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('submit', event => searchFilms(event));
+
+// функция поиска фильма
+function searchFilms(event) {
   // убрала дефолтное поведение формы
   event.preventDefault();
 
   //   Записываю в переменную inputValue значение записанное в инпут(название фильма которое ищут)
   inputValue = $input.value.trim();
-  // не удаётся достучатся до введённого слова в инпут
+  //  Делегированием не удаётся достучатся до введённого слова в инпут
   // const input = event.currentTarget;
   // достучалась до инпута (елемента формы)
   // console.dir(input.elements);
 
   // функция поиска фильма
   fetchFilms(inputValue);
-});
+}
 
-// функция поиска фильма
+// функция отправки запроса на API 
 function fetchFilms(inputValue) {
   // возвращаем из функции промис
-  return fetch('https://api.themoviedb.org/3/search/movie/?api_key=' + `${apiKey}`+'&query='+`${inputValue}`)
-    .then(responce => responce.json()).then(data=>console.log(data))
+  return fetch(
+    'https://api.themoviedb.org/3/search/movie/?api_key=' +
+      `${apiKey}` +
+      '&query=' +
+      `${inputValue}`,
+  )
+    .then(responce => responce.json())
+    .then(data => console.log(data))
     .catch(apiError => console.log(apiError));
 }
