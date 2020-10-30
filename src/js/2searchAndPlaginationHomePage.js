e6ff7933de2cf791780da361421123ebfb76a4e3
anastasiia-kolieva <nastya1110@gmail.com>
2020-10-30 18:17:33 +0200

    created code for cleaning ul $moviesList

--- src/js/2searchAndPlaginationHomePage.js
+++ src/js/2searchAndPlaginationHomePage.js
@@ -10,9 +10,11 @@ const searchForm = document.querySelector('form.search-form');
 // ссылка на инпут
 const $input = document.querySelector('.search-form__input');
 
+// ссылка на параграф с ошибкой
+const $searchFormError = document.querySelector('p.search-form__error');
+
 // На форму поставила слушатель событий
 searchForm.addEventListener('submit', event => searchFilms(event));
-
 
 // функция поиска фильма
 function searchFilms(event) {
@@ -26,16 +28,15 @@ function searchFilms(event) {
   // достучалась до инпута (елемента формы)
   // console.dir(input.elements);
 
-  // функция очистки результата поиска перед новым вводом поиска фильма 
-//   Артём обрати внимание, срабатывает ли и не ламает твою часть!!!
-  clearSearchFormInput();
+  // функция очистки результата поиска перед новым вводом поиска фильма
+  //   Артём обрати внимание, срабатывает ли и не ламает твою часть!!!
+  searchForm.reset();
 
   // функция поиска фильма
   fetchFilms(inputValue);
 }
 
-
-// функция отправки запроса на API 
+// функция отправки запроса на API
 function fetchFilms(inputValue) {
   // возвращаем из функции промис
   return fetch(
@@ -45,12 +46,24 @@ function fetchFilms(inputValue) {
       `${inputValue}`,
   )
     .then(responce => responce.json())
-    .then(data => console.log(data))
-    .catch(apiError => console.log(apiError));
-}
+    .then(movies => {
+      // в случае ответа пустым массивом отрисовывать ошибку
+      if (movies.results.length === 0) {
+        $searchFormError.classList.replace(
+          'search-form__error--hidden',
+          'search-form__error--visibale',
+        );
+      } else {
+        //   если массив не пустой пришёл
+        // в случае корректного ответа чистить ul
+        // Артём проверь плиз)
+        $moviesList.innerHTML = ' ';
 
-// функция очистки результата поиска перед новым вводом поиска фильма 
-//   Артём обрати внимание, срабатывает ли и не ламает твою часть!!!
-function clearSearchFormInput(){
-    $moviesList.innerHTML = ' ';
+
+        // !!!!!!и с помощью createCardFunc созданной первым участником отрисовывать фильмы, не забываем также положить в глобальную переменную renderFilms результат;
+        console.log(movies);
+        
+      }
+    })
+    .catch(apiError => console.log(apiError));
 }
