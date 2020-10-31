6ca2c77b90c6c73a090691582ae890f159b73241
transformator98 <vanushka16@gmail.com>
2020-10-31 17:25:44 +0200

    adedd icon luope

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -9,12 +9,14 @@ const refs = {
   libraryPage: document.querySelector('.js-library-page'),
   addBtnWatched: document.querySelector('button[data-add="btn-watched"]'),
   addBtnQueue: document.querySelector('button[data-add="btn-queue"]'),
+  prevBtn: document.querySelector('button[data-action="previous"]'),
+  nextBtn: document.querySelector('button[data-action="next"]'),
 };
 
 refs.logoRef.addEventListener('click', activeHomePage);
-refs.homeBtn.addEventListener('click', testClick);
-refs.libraryBtn.addEventListener('click', testClick);
-// refs.detailsPage.addEventListener('click', testClick);
+refs.homeBtn.addEventListener('click', activeHomePage);
+refs.libraryBtn.addEventListener('click', activeLibraryPage);
+refs.detailsPage.addEventListener('click', testClick);
 refs.homePage.addEventListener('click', testClick);
 refs.libraryPage.addEventListener('click', testClick);
 refs.addBtnWatched.addEventListener('click', testClick);
@@ -24,16 +26,27 @@ function testClick() {
   console.log('hello');
 }
 function activeHomePage() {
+  refs.homeBtn.classList.add('onClick');
+  refs.libraryBtn.classList.remove('onClick');
+
+  refs.detailsPage.classList.add('visually-hidden');
+
+  //Подключение пагинации
+  refs.prevBtn.addEventListener('click');
+  refs.nextBtn.addEventListener('click');
+
   //function
   //добавить домашнюю страницу
   // visualy-hiden для detailsPage
   // visualy-hiden для libraryPage
-  console.log('logo');
+  // console.log('logo');
 }
 
 function activeLibraryPage() {
-  //function
-  drawQueueFilmList();
+  refs.homeBtn.classList.remove('onClick');
+  refs.libraryBtn.classList.add('onClick');
+
+  // drawQueueFilmList();
 }
 
 function activeDetailsPage(movieId, itsLibraryFilm) {
